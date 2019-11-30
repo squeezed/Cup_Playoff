200aeb62c9410ce223b35142f27d81c8e176401b
squeezed <squeezed23@gmail.com>
2019-11-30 17:23:04 +1000

    hotfix

--- groups_6_by_6.py
+++ groups_6_by_6.py
@@ -17,25 +17,29 @@ def get_rating(record):
     return int(record.split('|')[1])
 
 
-with open('./config/groups_6_by_6.txt', 'r', encoding='utf8') as raw:
-    raw_players = [x for x in raw.read().splitlines() if x and not x.startswith('#')]
-result = []
+if __name__ == '__main__':
 
-# Сортируем игроков по рейтингу, далее распределяем по корзинам (в данном случае - 6 корзин по 6 игроков)
-players = sorted(raw_players, key=get_rating, reverse=True)
-players_by_buckets = [players[:6], players[6:12], players[12:18], players[18:24], players[24:30], players[30:]]
+    with open('./config/groups_6_by_6.txt', 'r', encoding='utf8') as raw:
+        raw_players = [x for x in raw.read().splitlines() if x and not x.startswith('#')]
+    result = []
 
-# Перемешиваем каждую из корзин для интриги:)
-for lst in players_by_buckets:
-    shuffle(lst)
+    # Сортируем игроков по рейтингу, далее распределяем по корзинам (в данном случае - 6 корзин по 6 игроков)
+    players = sorted(raw_players, key=get_rating, reverse=True)
+    players_by_buckets = [players[:6], players[6:12], players[12:18], players[18:24], players[24:30], players[30:]]
 
-# Записываем распределение по группам в файл и выводим на экран
-with open('./result/groups_6_by_6.txt', 'w', encoding='utf8') as out:
-    for idx in range(6):
-        out.write('Группа {}:\n'.format(LETTERS[idx]))
-        print('Группа {}:\n'.format(LETTERS[idx]))
-        for num_b, bucket in enumerate(players_by_buckets, start=1):
-            out.write('{}. {} - {}\n'.format(num_b, get_name(bucket[idx]), get_rating(bucket[idx])))
-            print('{}. {} - {}\n'.format(num_b, get_name(bucket[idx]), get_rating(bucket[idx])))
-        out.write('\n')
-        print('\n')
+    # Перемешиваем каждую из корзин для интриги:)
+    for lst in players_by_buckets:
+        shuffle(lst)
+
+    # Записываем распределение по группам в файл и выводим на экран
+    with open('./result/groups_6_by_6.txt', 'w', encoding='utf8') as out:
+        for idx in range(6):
+            out.write('Группа {}:\n'.format(LETTERS[idx]))
+            print('Группа {}:\n'.format(LETTERS[idx]))
+            for num_b, bucket in enumerate(players_by_buckets, start=1):
+                out.write('{}. {} - {}\n'.format(num_b, get_name(bucket[idx]), get_rating(bucket[idx])))
+                print('{}. {} - {}\n'.format(num_b, get_name(bucket[idx]), get_rating(bucket[idx])))
+            out.write('\n')
+            print('\n')
+
+input('Press Enetr to Exit...')
